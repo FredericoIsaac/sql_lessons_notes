9220641614604aa5a36bacdde95b1960515e6bae
Frederico <fredyisaac@gmail.com>
2021-03-12 07:12:42 +0000

    AGGREGATION AND OPERATOR

--- lesson_1.sql
+++ lesson_1.sql
@@ -241,3 +241,193 @@ JOIN accounts a
 ON a.id = o.account_id
 WHERE o.occurred_at BETWEEN '2015-01-01' AND '2015-12-31'
 ORDER BY o.occurred_at;
+
+/* Find the total amount of poster_qty paper ordered in the orders table */
+SELECT SUM(o.poster_qty) AS total_posters_sales
+FROM orders o;
+
+/* Find the total amount of standard_qty paper ordered in the orders table */
+SELECT SUM(o.standard_qty) AS total_standard_sales
+FROM orders o;
+
+/* Find the total dollar amount of sales using the total_amt_usd in the orders table */
+SELECT SUM(o.total_amt_usd) AS total_dollar_sales
+FROM orders o;
+
+/* Find the total amount spent on standard_amt_usd and gloss_amt_usd paper for each order in the orders table. This should give a dollar amount for each order in the table */
+SELECT o.standard_amt_usd + o.gloss_amt_usd total_amount_gloss_standard
+FROM orders o;
+
+/* Find the standard_amt_usd per unit of standard_qty paper. Your solution should use both an aggregation and a mathematical operator */
+SELECT SUM(o.standard_amt_usd)/SUM(o.standard_qty) AS standard_amt_usd_per_unit
+FROM orders o;
+
+/* When was the earliest order ever placed? You only need to return the date */
+SELECT MIN(o.occurred_at)
+FROM orders o;
+
+/* Try performing the same query as in question 1 without using an aggregation function */
+SELECT o.occurred_at
+FROM orders o
+ORDER BY o.occurred_at
+LIMIT 1;
+
+/* When did the most recent (latest) web_event occur */
+SELECT MAX(w.occurred_at)
+FROM web_events w;
+
+/* Try to perform the result of the previous query without using an aggregation function */
+SELECT w.occurred_at
+FROM web_events w
+ORDER BY w.occurred_at DESC
+LIMIT 1;
+
+/* Find the mean (AVERAGE) amount spent per order on each paper type, as well as the mean amount of each paper type purchased per order. Your final answer should have 6 values - one for each paper type for the average number of sales, as well as the average amount */
+SELECT AVG(o.standard_amt_usd), AVG(o.gloss_amt_usd), AVG(o.poster_amt_usd), AVG(o.standard_qty), AVG(o.gloss_qty), AVG(o.poster_qty)
+FROM orders o;
+
+/* What is the MEDIAN total_usd spent on all orders */
+SELECT PERCENTILE_CONT(0.5)
+WITHIN GROUP (ORDER BY o.total_amt_usd)
+FROM orders o;
+
+/* Which account (by name) placed the earliest order? Your solution should have the account name and the date of the order */
+SELECT a.name, MIN(o.occurred_at)
+FROM accounts a
+JOIN orders o
+ON a.id = o.account_id
+GROUP BY a.name, o.occurred_at
+ORDER BY o.occurred_at
+LIMIT 1;
+
+SELECT a.name, o.occurred_at
+FROM accounts a
+JOIN orders o
+ON a.id = o.account_id
+ORDER BY o.occurred_at
+LIMIT 1;
+
+/* Find the total sales in usd for each account. You should include two columns - the total sales for each company's orders in usd and the company name */
+SELECT a.name, SUM(o.total_amt_usd) total_sales
+FROM accounts a
+JOIN orders o
+ON a.id = o.account_id
+GROUP BY a.name
+ORDER BY total_sales DESC;
+
+/* Via what channel did the most recent (latest) web_event occur, which account was associated with this web_event? Your query should return only three values - the date, channel, and account name */
+SELECT MAX(w.occurred_at), w.channel, a.name
+FROM accounts a
+JOIN web_events w
+ON a.id = w.account_id
+GROUP BY w.channel, a.name, w.occurred_at
+ORDER BY w.occurred_at DESC
+LIMIT 1;
+
+SELECT w.occurred_at, w.channel, a.name
+FROM accounts a
+JOIN web_events w
+ON a.id = w.account_id
+ORDER BY w.occurred_at DESC
+LIMIT 1;
+
+/* Find the total number of times each type of channel from the web_events was used. Your final table should have two columns - the channel and the number of times the channel was used */
+SELECT w.channel, COUNT(*) n_times_channel
+FROM web_events w
+GROUP BY w.channel;
+
+/* Who was the primary contact associated with the earliest web_event */
+SELECT a.primary_poc, MIN(w.occurred_at)
+FROM accounts a
+JOIN web_events w
+ON w.account_id = a.id
+GROUP BY a.primary_poc, w.occurred_at
+ORDER BY w.occurred_at
+LIMIT 1;
+
+SELECT a.primary_poc
+FROM accounts a
+JOIN web_events w
+ON w.account_id = a.id
+ORDER BY w.occurred_at
+LIMIT 1;
+
+/* What was the smallest order placed by each account in terms of total usd. Provide only two columns - the account name and the total usd. Order from smallest dollar amounts to largest */
+SELECT a.name, MIN(o.total_amt_usd) AS total_usd
+FROM accounts a
+JOIN orders o
+ON a.id = o.account_id
+WHERE o.total_amt_usd > 0
+GROUP BY a.name
+ORDER BY total_usd;
+
+/* Find the number of sales reps in each region. Your final table should have two columns - the region and the number of sales_reps. Order from fewest reps to most reps */
+SELECT r.name region, COUNT(s.name) sales_reps
+FROM region r
+JOIN sales_reps s
+ON r.id = s.region_id
+GROUP BY r.name
+ORDER BY sales_reps;
+
+/* For each account, determine the average amount of each type of paper they purchased across their orders. Your result should have four columns - one for the account name and one for the average quantity purchased for each of the paper types for each account */
+SELECT a.name, AVG(o.standard_qty) avg_standard, AVG(o.gloss_qty) avg_gloss, AVG(o.poster_qty) avg_poster
+FROM accounts a
+JOIN orders o
+ON a.id = o.account_id
+GROUP BY a.name;
+
+/* For each account, determine the average amount spent per order on each paper type. Your result should have four columns - one for the account name and one for the average amount spent on each paper type */
+SELECT a.name, AVG(o.standard_amt_usd) avg_standard, AVG(o.gloss_amt_usd) avg_gloss, AVG(o.poster_amt_usd) avg_poster
+FROM accounts a
+JOIN orders o
+ON a.id = o.account_id
+GROUP BY a.name;
+
+/* Determine the number of times a particular channel was used in the web_events table for each sales rep. Your final table should have three columns - the name of the sales rep, the channel, and the number of occurrences. Order your table with the highest number of occurrences first */
+SELECT s.name rep, w.channel, COUNT(*) n_times
+FROM sales_reps s
+JOIN accounts a
+ON s.id = a.sales_rep_id
+JOIN web_events w
+ON a.id = w.account_id
+GROUP BY s.name, w.channel
+ORDER BY n_times DESC;
+
+/* Determine the number of times a particular channel was used in the web_events table for each region. Your final table should have three columns - the region name, the channel, and the number of occurrences. Order your table with the highest number of occurrences first. */
+SELECT r.name region, w.channel, COUNT(*) n_times
+FROM region r
+JOIN sales_reps s
+ON r.id = s.region_id
+JOIN accounts a
+ON s.id = a.sales_rep_id
+JOIN web_events w
+ON w.account_id = a.id
+GROUP BY r.name, w.channel
+ORDER BY n_times DESC;
+
+/* Use DISTINCT to test if there are any accounts associated with more than one region */
+/* The below two queries have the same number of resulting rows (351), so we know that every account is associated with only one region. */
+SELECT a.id as "account id", r.id as "region id", 
+a.name as "account name", r.name as "region name"
+FROM accounts a
+JOIN sales_reps s
+ON s.id = a.sales_rep_id
+JOIN region r
+ON r.id = s.region_id;
+
+SELECT DISTINCT id, name
+FROM accounts;
+
+/* Have any sales reps worked on more than one account */
+/* Using DISTINCT in the second query assures that all of the sales reps are accounted for in the first query */
+SELECT s.id, s.name, COUNT(*) num_accounts
+FROM accounts a
+JOIN sales_reps s
+ON s.id = a.sales_rep_id
+GROUP BY s.id, s.name
+ORDER BY num_accounts;
+
+SELECT DISTINCT id, name
+FROM sales_reps;
+
+
