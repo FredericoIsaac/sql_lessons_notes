6ef1b59fe4dfe4fec0774345b15e4b2dda31031e
Frederico <fredyisaac@gmail.com>
2021-03-15 17:09:24 +0000

    advance SQL

--- lesson_1.sql
+++ lesson_1.sql
@@ -983,4 +983,114 @@ WITH t1 AS (
 SELECT primary_poc,
 	name,
 	con_pass_1 || con_pass_2 || con_pass_3 || con_pass_4 || con_pass_5 || con_pass_6 || con_pass_7 AS password
-FROM t2
+FROM t2;
+
+/* create a running total of standard_amt_usd (in the orders table) over order time with no date truncation. Your final table should have two columns: one with the amount being added for each new row, and a second with the running total */
+SELECT standard_amt_usd,
+SUM(standard_amt_usd) OVER (ORDER BY occurred_at) AS running_total
+FROM orders;
+
+/* Now, modify your query from the previous quiz to include partitions. Still create a running total of standard_amt_usd (in the orders table) over order time, but this time, date truncate occurred_at by year and partition by that same year-truncated occurred_at variable. Your final table should have three columns: One with the amount being added for each row, one for the truncated date, and a final column with the running total within each year */
+SELECT standard_amt_usd,
+	DATE_TRUNC('year', occurred_at) AS year,
+	SUM(standard_amt_usd) OVER (PARTITION BY DATE_TRUNC('year', occurred_at) ORDER BY occurred_at) AS running_total
+FROM orders;
+
+/* Select the id, account_id, and total variable from the orders table, then create a column called total_rank that ranks this total amount of paper ordered (from highest to lowest) for each account using a partition. Your final table should have these four columns */
+SELECT id, account_id, total,
+	RANK() OVER (PARTITION BY account_id ORDER BY total DESC) as total_rank
+FROM orders;
+
+/* create and use an alias to shorten the following query (which is different than the one in Derek's previous video) that has multiple window functions. Name the alias account_year_window, which is more descriptive than main_window in the example above */
+SELECT id,
+       account_id,
+       DATE_TRUNC('year',occurred_at) AS year,
+       DENSE_RANK() OVER (PARTITION BY account_id ORDER BY DATE_TRUNC('year',occurred_at)) AS dense_rank,
+       total_amt_usd,
+       SUM(total_amt_usd) OVER account_year_window AS sum_total_amt_usd,
+       COUNT(total_amt_usd) OVER account_year_window AS count_total_amt_usd,
+       AVG(total_amt_usd) OVER account_year_window AS avg_total_amt_usd,
+       MIN(total_amt_usd) OVER account_year_window AS min_total_amt_usd,
+       MAX(total_amt_usd) OVER account_year_window AS max_total_amt_usd
+FROM orders
+WINDOW account_year_window AS (PARTITION BY account_id ORDER BY DATE_TRUNC('year',occurred_at));
+
+/* Imagine you're an analyst at Parch & Posey and you want to determine how the current order's total revenue ("total" meaning from sales of all types of paper) compares to the next order's total revenue. Modify Derek's query from the previous video in the SQL Explorer below to perform this analysis. You'll need to use occurred_at and total_amt_usd in the orders table along with LEAD to do so. In your query results, there should be four columns: occurred_at, total_amt_usd, lead, and lead_difference. */
+SELECT occurred_at,
+       total,
+       LEAD(total) OVER (ORDER BY occurred_at) AS lead,
+       LEAD(total) OVER (ORDER BY occurred_at) - total AS lead_difference
+FROM (
+SELECT occurred_at,
+       SUM(total_amt_usd) AS total
+  FROM orders 
+ GROUP BY 1
+ ) sub;
+ 
+ /* Use the NTILE functionality to divide the accounts into 4 levels in terms of the amount of standard_qty for their orders. Your resulting table should have the account_id, the occurred_at time for each order, the total amount of standard_qty paper purchased, and one of four levels in a standard_quartile column */
+ SELECT account_id,
+ 	occurred_at,
+	standard_qty,
+	NTILE(4) OVER (PARTITION BY account_id ORDER BY standard_qty) AS standard_quartile
+FROM orders
+ORDER BY account_id DESC;
+
+/* Use the NTILE functionality to divide the accounts into two levels in terms of the amount of gloss_qty for their orders. Your resulting table should have the account_id, the occurred_at time for each order, the total amount of gloss_qty paper purchased, and one of two levels in a gloss_half column */
+SELECT account_id,
+ 	occurred_at,
+	gloss_qty,
+	NTILE(2) OVER (PARTITION BY account_id ORDER BY gloss_qty) AS gloss_qhalf
+FROM orders
+ORDER BY account_id DESC;
+
+/* Use the NTILE functionality to divide the orders for each account into 100 levels in terms of the amount of total_amt_usd for their orders. Your resulting table should have the account_id, the occurred_at time for each order, the total amount of total_amt_usd paper purchased, and one of 100 levels in a total_percentile column */
+SELECT account_id,
+ 	occurred_at,
+	total_amt_usd,
+	NTILE(100) OVER (PARTITION BY account_id ORDER BY total_amt_usd) AS total_percentile
+FROM orders
+ORDER BY account_id DESC;
+
+/* each account who has a sales rep and each sales rep that has an account (all of the columns in these returned rows will be full), but also each account that does not have a sales rep and each sales rep that does not have an account (some of the columns in these returned rows will be empty) */
+SELECT *
+FROM sales_reps
+FULL JOIN accounts
+ON sales_reps.id = accounts.sales_rep_id;
+
+/* In the following SQL Explorer, write a query that left joins the accounts table and the sales_reps tables on each sale rep's ID number and joins it using the < comparison operator on accounts.primary_poc and sales_reps.name The query results should be a table with three columns: the account name (e.g. Johnson Controls), the primary contact name (e.g. Cammy Sosnowski), and the sales representative's name (e.g. Samuel Racine) */
+SELECT a.name, 
+	a.primary_poc,
+	s.name
+FROM accounts a
+LEFT JOIN sales_reps s
+ON a.sales_rep_id = s.id 
+AND a.primary_poc < s.name;
+
+/* change the interval to 1 day to find those web events that occurred after, but not more than 1 day after, another web event, add a column for the channel variable in both instances of the table in your query */
+SELECT w1.id AS w1_id,
+       w1.account_id AS w1_account_id,
+       w1.occurred_at AS w1_occurred_at,
+	   w1.channel AS w1_channel,
+       w2.id AS w2_id,
+       w2.account_id AS w2_account_id,
+       w2.occurred_at AS w2_occurred_at,
+	   w2.channel AS w2_channel
+  FROM web_events w1
+ LEFT JOIN web_events w2
+   ON w1.account_id = w2.account_id
+  AND w1.occurred_at > w2.occurred_at
+  AND w1.occurred_at <= w2.occurred_at + INTERVAL '1 days'
+ORDER BY w1.account_id, w2.occurred_at;
+
+
+
+
+
+
+
+
+
+
+
+
+
