f7a38cddb51be26355d2d851f1257da0c1d77e9a
Frederico <fredyisaac@gmail.com>
2021-03-15 07:03:00 +0000

    CONCAT AND data cleaning

--- lesson_1.sql
+++ lesson_1.sql
@@ -878,4 +878,109 @@ WITH t1 AS (
 SELECT AVG(t2.total_spent)
 FROM t2;
 
+/* In the accounts table, there is a column holding the website for each company. The last three digits specify what type of web address they are using. Pull these extensions and provide how many of each website type exist in the accounts table */
+WITH t1 AS (
+	SELECT website,
+	RIGHT(website, 3) AS net_address
+	FROM accounts)
 
+SELECT net_address, COUNT(net_address) count_domain
+FROM t1
+GROUP BY net_address
+ORDER BY count_domain;
+
+/* There is much debate about how much the name (or even the first letter of a company name) matters. Use the accounts table to pull the first letter of each company name to see the distribution of company names that begin with each letter (or number) */
+WITH t1 AS(
+	SELECT name,
+	LEFT(name, 1) AS letter_dist
+	FROM accounts
+	)
+
+SELECT letter_dist, COUNT(letter_dist)
+FROM t1
+GROUP BY letter_dist
+ORDER BY letter_dist;
+
+/* Use the accounts table and a CASE statement to create two groups: one group of company names that start with a number and a second group of those company names that start with a letter. What proportion of company names start with a letter */
+WITH t1 AS (
+	SELECT name,
+	CASE WHEN LEFT(name, 1) IN ('0', '1', '2', '3', '4', '5', '6', '7', '8', '9') THEN 1 ELSE 0 END AS num,
+	CASE WHEN LEFT(UPPER(name), 1) IN ('0', '1', '2', '3', '4', '5', '6', '7', '8', '9') THEN 0 ELSE 1 END AS letter
+	FROM accounts
+		)
+SELECT SUM(num) nums, SUM(letter) letters
+FROM t1;
+
+/* Consider vowels as a, e, i, o, and u. What proportion of company names start with a vowel, and what percent start with anything else */
+WITH t1 AS (
+	SELECT name,
+	CASE WHEN LEFT(UPPER(NAME), 1) IN ('A', 'E', 'I', 'O', 'U') THEN 1 ELSE 0 END AS vowels,
+	CASE WHEN LEFT(UPPER(NAME), 1) IN ('A', 'E', 'I', 'O', 'U') THEN 0 ELSE 1 END AS not_vowels
+	FROM accounts
+	)
+
+SELECT SUM(vowels) AS vowels, SUM(not_vowels) AS other_letters
+FROM t1;
+
+/* Use the accounts table to create first and last name columns that hold the first and last names for the primary_poc */
+SELECT primary_poc,
+	POSITION(' ' IN primary_poc) AS space_pos,
+	LEFT(primary_poc, POSITION(' ' IN primary_poc) -1) as first_name,
+	RIGHT(primary_poc, LENGTH(primary_poc) - POSITION(' ' IN primary_poc)) as last_name
+FROM accounts;
+
+/* Now see if you can do the same thing for every rep name in the sales_reps table. Again provide first and last name columns */
+SELECT LEFT(name, POSITION(' ' IN name)) as first_name,
+	RIGHT(name, LENGTH(name) - POSITION(' ' IN name)) as last_name
+FROM sales_reps;
+
+/* Each company in the accounts table wants to create an email address for each primary_poc. The email address should be the first name of the primary_poc . last name primary_poc @ company name .com */
+WITH t1 AS (
+	SELECT primary_poc, name,
+	LEFT(primary_poc, POSITION(' ' IN primary_poc) -1) as first_name,
+	RIGHT(primary_poc, LENGTH(primary_poc) - POSITION(' ' IN primary_poc)) as last_name
+	FROM accounts
+	)
+
+SELECT primary_poc,
+	name,
+	CONCAT(LOWER(first_name),'.',LOWER(last_name),'@',LOWER(name),'.com')
+	FROM t1;
+
+/* You may have noticed that in the previous solution some of the company names include spaces, which will certainly not work in an email address. See if you can create an email address that will work by removing all of the spaces in the account name, but otherwise your solution should be just as in question 1 */
+WITH t1 AS (
+	SELECT primary_poc, name,
+	LEFT(primary_poc, POSITION(' ' IN primary_poc) -1) as first_name,
+	RIGHT(primary_poc, LENGTH(primary_poc) - POSITION(' ' IN primary_poc)) as last_name,
+	REPLACE(name, ' ', '_') AS new_name
+	FROM accounts
+	)
+
+SELECT primary_poc,
+	name,
+	CONCAT(LOWER(first_name),'.',LOWER(last_name),'@',LOWER(new_name),'.com')
+	FROM t1;
+
+/* We would also like to create an initial password, which they will change after their first log in. The first password will be the first letter of the primary_poc's first name (lowercase), then the last letter of their first name (lowercase), the first letter of their last name (lowercase), the last letter of their last name (lowercase), the number of letters in their first name, the number of letters in their last name, and then the name of the company they are working with, all capitalized with no spaces */
+WITH t1 AS (
+	SELECT primary_poc, name,
+	LEFT(primary_poc, POSITION(' ' IN primary_poc) -1) as first_name,
+	RIGHT(primary_poc, LENGTH(primary_poc) - POSITION(' ' IN primary_poc)) as last_name
+	FROM accounts
+	),
+	t2 AS (
+	SELECT primary_poc, name,
+		LEFT(LOWER(first_name), 1) AS con_pass_1,
+		RIGHT(LOWER(first_name), 1) AS con_pass_2,
+		LEFT(LOWER(last_name), 1) AS con_pass_3,
+		RIGHT(LOWER(last_name), 1) AS con_pass_4,
+		LENGTH(first_name) AS con_pass_5,
+		LENGTH(last_name) AS con_pass_6,
+		REPLACE(UPPER(name), ' ', '') AS con_pass_7
+	FROM t1
+	)
+
+SELECT primary_poc,
+	name,
+	con_pass_1 || con_pass_2 || con_pass_3 || con_pass_4 || con_pass_5 || con_pass_6 || con_pass_7 AS password
+FROM t2
